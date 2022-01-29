c1f00a119058617b36b1650511e39392d350fd7d
MadsonSantosCe <madsonsamsung@gmail.com>
2022-01-29 16:03:08 -0300

    First Commit

---
+++ rest_api/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using rest_api.Persistence.Contexts;
+
+namespace rest_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildeWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
+        }        
+
+        public static IWebHost BuildeWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+}
